772ce0ed47365e029537ae3bacc507185938075e
Adam Jahr <adamjahr@Adams-MacBook-Pro.local>
2022-06-28 22:22:13 -0400

    cleaned smart contract

--- contracts/Counter.sol
+++ contracts/Counter.sol
@@ -2,21 +2,22 @@
 pragma solidity ^0.8.4;
 
 contract Counter {
+    // data stored on blockchain
     uint public count;
 
-    // Function to get the current count
+    // get the current count
     function getCount() public view returns (uint) {
         return count;
     }
 
-    // Function to increment count by 1
+    // increment count by 1
     function incrementCount() public {
         count += 1;
     }
 
-    // Function to decrement count by 1
+    // decrement count by 1
     function decrementCount() public {
-        // This function will fail if count = 0
+        // will fail if count = 0
         count -= 1;
     }
 }
